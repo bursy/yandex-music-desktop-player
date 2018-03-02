719de088644fed9bb2ba49a77f12585187ce31b2
Alex Ilyin <bursy@yandex-team.ru>
2018-03-02 18:16:10 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+//const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: false
+        }
+    })
+
+    mainWindow.loadURL(url.format({
+        pathname: 'music.yandex.ru',
+        protocol: 'https:',
+        slashes: true
+    }))
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
