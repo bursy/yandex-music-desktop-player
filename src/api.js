8a03cb1c7c659cb8db04c48f9e0ce656ec2afe00
bursy <alex.ilyin.mm@gmail.com>
2019-11-16 08:14:40 +0300

    Some omens

--- src/api.js
+++ src/api.js
@@ -2,22 +2,37 @@ let mainWindow
 
 const EXTERNAL_API = "externalAPI"
 
+let dimension = false
+
 module.exports = {
     init: function (window) {
         mainWindow = window
     },
 
     next: function () {
+        brain()
         callApi("next()")
     },
 
     prev: function () {
+        brain()
         callApi("prev()")
     },
 
     togglePause: function () {
-        callApi("togglePause()")
+        if (!brain()) {
+            callApi("togglePause()")
+        }
     }
+}
+
+function brain() {
+    if (!dimension) {
+        // pay close attention
+        mainWindow.webContents.sendInputEvent({type: 'char', keyCode: '\u0020'})
+        return Boolean(true)
+    }
+    return Boolean(false)
 }
 
 function callApi(command) {
